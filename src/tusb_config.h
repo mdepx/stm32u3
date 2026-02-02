c4967ce91add2302839a48ae78163fea50c31e55
Ruslan Bukin <br@bsdpad.com>
2026-02-02 16:17:37 +0000

    remove mem_section

--- src/tusb_config.h
+++ src/tusb_config.h
@@ -1,23 +1,18 @@
 #ifndef _SRC_TUSB_CONFIG_H_
 #define _SRC_TUSB_CONFIG_H_
 
-#define	BOARD_TUD_RHPORT	0
 #define	CFG_TUSB_MCU		OPT_MCU_STM32U3
 #define	CFG_TUSB_OS		OPT_OS_CUSTOM
-#define	CFG_TUSB_DEBUG		1
-#define	CFG_TUSB_MEM_SECTION	__attribute__ ((section(".usb_ram")))
-#define	CFG_TUSB_MEM_ALIGN	__attribute__ ((aligned(4)))
+#define	CFG_TUSB_DEBUG		10
 
 /* Device stack. */
-#define CFG_TUD_ENABLED			1
-#define	CFG_TUD_MAX_SPEED		OPT_MODE_DEFAULT_SPEED
-#define	CFG_TUD_ENDPOINT0_SIZE		64
+#define CFG_TUD_ENABLED		1
+#define CFG_TUD_CDC		0
+#define CFG_TUD_MSC		0
+#define CFG_TUD_HID		1
+#define CFG_TUD_MIDI		0
+#define CFG_TUD_VENDOR		0
 
-#define CFG_TUD_CDC			0
-#define CFG_TUD_MSC			0
-#define CFG_TUD_HID			1
-#define CFG_TUD_MIDI			0
-#define CFG_TUD_VENDOR			0
-#define	CFG_TUD_HID_EP_BUFSIZE		64
+#define	BOARD_TUD_RHPORT	0
 
 #endif /* _SRC_TUSB_CONFIG_H_ */
