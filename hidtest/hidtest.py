fb52c460c02e739f97d9777a06c3aa7019a4330a
Ruslan Bukin <br@bsdpad.com>
2026-02-02 19:59:44 +0000

    improve test code

--- hidtest/hidtest.py
+++ hidtest/hidtest.py
@@ -1,9 +1,15 @@
 import hidapi
+import os
+import sys
 
 d = hidapi.Device(vendor_id=0xCafe, product_id=0x4004)
 
-b = b'033'
-
-d.write(b)
-
-print("Return: %s" % d.read(3))
+while (1):
+	b = os.urandom(64)
+	d.write(b)
+	r = d.read(64)
+	if (b != r):
+		print("Failed")
+	else:
+		sys.stdout.write(".")
+		sys.stdout.flush()
